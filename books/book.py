f2d91c1fc5c62f4030d01fe8ace00edd1cb13966
Syrkov Michael <syrf.pro@gmail.com>
2024-03-10 04:38:11 +0300

    Structure and MyBookSource

--- books/book.py
+++ books/book.py
@@ -1,13 +1,16 @@
+from urllib.parse import quote_plus as quote
+import requests
+from typing import NamedTuple
 
 
-
-
-class Book:
-    pass
+class Book(NamedTuple):
+    title: str
+    url: str
+    # short_description: [None, str]
 
 
 class Source:
-    title: str = ''
+    host: str = ''
     books: list = []
     timeout: int = 10
 
@@ -15,12 +18,31 @@ class Source:
     async def get_books(cls):
         pass
 
-    def search_by_title():
+    @classmethod
+    def search_by_title(cls, book_title):
         pass
 
-    def search_by_author():
+    @classmethod
+    def search_by_author(cls):
         pass
 
+
+class MyBookSource(Source):
+    host = 'mybook.ru'
+
+    @classmethod
+    def search_by_title(cls, book_title):
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+        }
+        url_template = f'https://mybook.ru/search/?q={quote(book_title)}'
+        response = requests.get(url_template, headers=headers)
+        html = response.text
+
+        def parse_books(_html) -> list[type[Book]]:
+            pass
+
+        books = parse_books(html)
 
 
 
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from flask import Flask, render_template
+from quart import Quart, render_template, websocket
+
+app = Quart(__name__)
 
 
-app = Flask(__name__)
+@app.route("/")
+async def hello():
+    return await render_template("ws.html")
 
 
-@app.route('/hello/<name>')
-def hello(name='default'):
-    return render_template('hello.html', name=name)
+@app.route("/api")
+async def json():
+    return {"hello": "world"}
+
+
+@app.websocket("/ws")
+async def ws():
+    while True:
+        data = await websocket.receive()
+        await websocket.send(f"hello {data}")
+        await websocket.send_json({"hello": data})
+
+if __name__ == "__main__":
+    app.run()
